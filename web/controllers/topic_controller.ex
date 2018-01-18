a07ef6732b18989c89cca141d993657390ded85c
Adriel Radicchi <adriel.nfsc@gmail.com>
2018-01-18 18:07:29 -0200

    topic.oauth
    Add a new deps for project

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -45,7 +45,7 @@ defmodule Discuss.TopicController do
                 render conn, "edit.html", changeset: changeset, topic: old_topic
         end
      end
-
+     
     def delete(conn, %{"id" => topic_id})  do
         Repo.get!(Topic, topic_id) |> Repo.delete!
         
